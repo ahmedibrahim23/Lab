9e9bf868015629a1ed051d0adf2b3a225e35e200
ahmed_hefow <axmedaadan484@gmail.com>
2024-09-22 20:01:56 +0100

    home page added

---
+++ Mobile/test_app/lib/HomePage.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Welcome"),
+        backgroundColor: Colors.lightBlueAccent,
+        actions: [Icon(Icons.search)],
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.blueAccent,
+                child: Column(
+                  children: [
+                    Image.network(
+                        "https://gratisography.com/wp-content/uploads/2024/03/gratisography-vr-glasses-1170x780.jpg"),
+                    Text("100")
+                  ],
+                ),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: const Color.fromARGB(255, 68, 255, 99),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: const Color.fromARGB(255, 124, 68, 255),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: const Color.fromARGB(255, 255, 174, 68),
+              ),
+            ],
+          ),
+          Text("Kuwani Box larabo in labuxiyo"),
+          Image.network(
+              "https://gratisography.com/wp-content/uploads/2024/03/gratisography-vr-glasses-1170x780.jpg")
+        ],
+      ),
+    );
+  }
+}
